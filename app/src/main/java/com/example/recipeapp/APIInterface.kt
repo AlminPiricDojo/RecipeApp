10a51d405d8427d43bfbea4c420509fcf22547ca
TMDStudios <tmdstudioscontact@gmail.com>
2021-12-19 20:10:37 -0500

    Updated API Interface

--- app/src/main/java/com/example/recipeapp/APIInterface.kt
+++ app/src/main/java/com/example/recipeapp/APIInterface.kt
@@ -1,13 +1,19 @@
 package com.example.recipeapp
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface APIInterface {
+    // This function returns a Call (we have to use the await or awaitResponse function)
     @GET("recipes/")
     fun getAll(): Call<List<Recipe>>
+
+    // This function returns a Response (we no longer need the await function)
+    @GET("recipes/")
+    suspend fun getAllResponse(): Response<List<Recipe>>
 
     @POST("recipes/")
     fun addRecipe(@Body recipeData: Recipe): Call<Recipe>
