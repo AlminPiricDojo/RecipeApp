f2bda787bd7087e0196b4eb47ac5f2cc426dd9a8
TMDStudios <tmdstudioscontact@gmail.com>
2022-09-01 16:19:44 -0400

    Added comments

--- app/src/main/java/com/example/recipeapp/RecipeAppModule.kt
+++ app/src/main/java/com/example/recipeapp/RecipeAppModule.kt
@@ -9,10 +9,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(SingletonComponent::class) // We use the SingletonComponenet here to keep our dependencies alive throughout the lifecycle of our application
 object RecipeAppModule {
-    @Provides
-    @Singleton
+    @Provides // Tells Dagger-Hilt that this function provides a dependency
+    @Singleton // Sets the scope of our dependency, creating only one instance throughout the lifetime of our application
     fun provideAPIInterface(): APIInterface {
         return Retrofit.Builder()
             .baseUrl("https://apidojo.pythonanywhere.com/")
